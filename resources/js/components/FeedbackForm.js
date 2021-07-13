0ee70cea2bb479bfac397b1480e90bb482a70a83
Brett Shepley <BrettShepley@gmail.com>
2021-07-12 20:39:03 -0400

    Update FeedbackForm.js

--- resources/js/components/FeedbackForm.js
+++ resources/js/components/FeedbackForm.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
-import {Grid, Form, Header, Rating, Button} from "semantic-ui-react";
+import {Grid, Form, Header, Rating, Button, Radio} from "semantic-ui-react";
 import "../../css/FeedbackPage.css";
 
 const FeedbackForm = () => {
 
     const [textFeedback, setTextFeedback] = useState('');
     const [intFeedback, setIntFeedback] = useState(0);
+    const [reachoutEmail, setReachoutEmail] = useState('');
+    const [willReachout, setWillReachout] = useState(false);
 
     const fullFeedback = () => {
         console.log(textFeedback);
         console.log(intFeedback);
+        console.log(reachoutEmail);
+        console.log(willReachout);
     }
 
     return(
@@ -23,7 +27,7 @@ const FeedbackForm = () => {
                                     <Header content="What is on your mind?" className='ss-feedback-header'/>
                                     <Form className="ss-feedback-form">
                                         <Form.TextArea
-                                        onChange={(e, {value}) => setTextFeedback(value)}
+                                        onChange={(e, {value}) => {setTextFeedback(value)}}
                                         className='ss-feedback-formtextarea'
                                         placeholder='Enter feedback here: '
                                         />
@@ -31,6 +35,21 @@ const FeedbackForm = () => {
                                     <div className='ss-feedback-starslabel'>
                                         <label> Please Enter a Rating: </label>
                                         <Rating icon='star' defaultRating={intFeedback} maxRating={5} onRate={(e, {rating})=>{setIntFeedback(rating)}}/>
+                                    </div>
+                                    <div>
+                                        <Form>
+                                            <label>Get E-mail of report</label>
+                                            <Radio 
+                                                slider
+                                                 onChange={() => {setWillReachout(!willReachout)}}
+                                            />
+                                            <Form.TextArea
+                                            onChange={(e, {value}) => {setReachoutEmail(value)}}
+                                            className='ss-feedback-formtextarea'
+                                            placeholder='Enter E-mail Here: '
+                                            />
+                                            
+                                        </Form>
                                     </div>
                                     <div className='ss-feedback-submitbutton'>
                                         <Button content="Submit Feedback" onClick={()=>{fullFeedback()}}/>
