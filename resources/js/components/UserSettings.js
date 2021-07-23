bc3be42c2ebb2766fd1f8b80913b27ae474aa563
Brett Shepley <BrettShepley@gmail.com>
2021-07-22 22:29:13 -0400

    Bug-Fixes

--- resources/js/components/UserSettings.js
+++ resources/js/components/UserSettings.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState, useEffect } from "react";
 import { Segment, Divider, Container, Header, Grid, Form, Image, Button, Icon } from 'semantic-ui-react';
 
